54f301f1df9744cf8e4dd865780652ba02d7b158
karthik_mu <karthik.mu@oracle.com>
2017-07-29 15:40:08 +0530

    Added Google Analytics and Linkedin badge.

--- js/custom.js
+++ js/custom.js
@@ -7,10 +7,31 @@
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    Typed.new("#element", {
-        strings: ["Karthik is Awesome Guy!!.", "trust me!"],
+    Typed.new("#nameElement", {
+        strings: ["Hi, I'm Karthik."],
         typeSpeed: 0,
         backDelay: 2000,
-        loop: true
+        loop: false,
+        showCursor: false,
+        onStringTyped: function () {
+            Typed.new("#contentElement", {
+                strings: [" Geek, ^500 programmer ^500 and a positive guy. ^500 Never short on ideas. ^500 Love to Code, ^500 learn and collaborate."],
+                typeSpeed: 0,
+                startDelay: 1000,
+                backDelay: 2000,
+                loop: true
+            });
+        }
     });
+
+    /*
+     Typed.new("#contentElement", {
+     strings: ["echo \'Geek\'", "echo \'Programmer\'", "echo \'Developer\'"],
+     typeSpeed: 0,
+     startDelay: 1000,
+     backDelay: 2000,
+     loop: true
+     }); */
+
+
 });
