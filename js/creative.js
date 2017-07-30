bbe487d573816d8867b1399d0537f66139cd308f
karthik_mu <karthik.mu@oracle.com>
2017-07-30 14:16:20 +0530

    Modifications and styles

--- js/creative.js
+++ js/creative.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(document).on('click', 'a.page-scroll', function(event) {
+    $(document).on('click', 'a.page-scroll', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
@@ -17,7 +17,7 @@
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-collapse ul li a').click(function () {
         $('.navbar-toggle:visible').click();
     });
 
@@ -46,19 +46,46 @@
     }, 300);
 
     // Initialize and Configure Magnific Popup Lightbox Plugin
+    /*
     $('.popup-gallery').magnificPopup({
         delegate: 'a',
-        type: 'image',
         tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        mainClass: 'mfp-with-fade'
+    }); */
+
+    // Hinge effect popup
+    $('a.hinge').magnificPopup({
+        mainClass: 'mfp-with-fade',
+        removalDelay: 1000, //delay removal by X to allow out-animation
+        callbacks: {
+            beforeClose: function () {
+                this.content.addClass('hinge');
+            },
+            close: function () {
+                this.content.removeClass('hinge');
+            }
         },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
+        midClick: true
     });
+
+$('.popup-gallery .video').magnificPopup({
+  type: 'iframe',
+  
+  
+  iframe: {
+     markup: '<div class="mfp-iframe-scaler">'+
+                '<div class="mfp-close"></div>'+
+                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                '<div class="mfp-title">Some caption</div>'+
+              '</div>'
+  },
+  callbacks: {
+    markupParse: function(template, values, item) {
+     values.title = item.el.attr('title');
+    }
+  }
+  
+  
+});
 
 })(jQuery); // End of use strict
--- js/custom.js
+++ js/custom.js
@@ -8,18 +8,18 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     Typed.new("#nameElement", {
-        strings: ["Hi, I'm Karthik."],
+        strings: ["Hi, I'm Karthik. <i class='fa fa-hand-paper-o' aria-hidden='true'></i>"],
         typeSpeed: 0,
         backDelay: 2000,
         loop: false,
         showCursor: false,
         onStringTyped: function () {
             Typed.new("#contentElement", {
-                strings: [" Geek, ^500 programmer ^500 and a positive guy. ^500 Never short on ideas. ^500 Love to Code, ^500 learn and collaborate."],
-                typeSpeed: 0,
-                startDelay: 1000,
+                strings: ["# ^500 Geek, programmer and a positive guy. ^500 Never short on ideas. ^500 <i class='fa fa-heart' aria-hidden='true'></i> to Code, ^500 learn and collaborate."],
+                typeSpeed: 5,
+                startDelay: 0,
                 backDelay: 2000,
-                loop: true
+                loop: false
             });
         }
     });
